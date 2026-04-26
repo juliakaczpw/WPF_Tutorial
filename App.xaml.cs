0cb486629e4c5742d6b3ffd36416faddabe8429c
Julia Kaczyńska <juliakaczynska.pw@gmail.com>
2026-04-27 00:35:45 +0200

    data class an basics

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_tut
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_tut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Contacts(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Contacts(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_About(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
